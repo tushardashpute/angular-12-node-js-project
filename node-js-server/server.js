6218e86cd72a46f5fa0aa01c3853d560afa0c65d
tushardashpute <74225291+tushardashpute@users.noreply.github.com>
2022-03-26 11:04:11 +0530

    Update server.js

--- node-js-server/server.js
+++ node-js-server/server.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(express.static(path));
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:80"
 };
 
 app.use(cors(corsOptions));
